b1bcd6d73d3c72235fa78d5d1fe45f1483e5bd02
Siddharth-M-77 <siddharthmaddheshiya777@gmail.com>
2024-11-13 23:24:47 +0530

    done

--- src/components/StoryView.jsx
+++ src/components/StoryView.jsx
@@ -34,14 +34,17 @@ const StoryView = ({ stories, setStories }) => {
   return (
     story && (
       <div className="fixed inset-0 bg-black flex items-center justify-center p-4">
-        <div className="relative w-full max-w-lg bg-white rounded-lg p-4">
+        <div className="w-full max-w-2xl bg-white rounded-lg p-4">
           <img
             src={story.file}
             alt="Story"
             className={`w-full h-auto rounded ${story.filter}`}
           />
           <div className="flex justify-center items-center mt-4">
-            <button onClick={handleDelete} className="text-red-500">
+            <button
+              onClick={handleDelete}
+              className="text-white rounded-lg px-6 py-2 bg-red-500 hover:bg-red-600"
+            >
               Delete
             </button>
           </div>
